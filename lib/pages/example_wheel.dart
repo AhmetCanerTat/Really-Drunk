d199c7191cef6b60fa93c255763bea710e81477b
Inicky1 <iannicholson2002@users.noreply.github.com>
2023-11-17 22:02:56 +0100

    Working Wheel

---
+++ lib/pages/example_wheel.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+
+class WheelComponent extends StatefulWidget {
+  @override
+  // ignore: library_private_types_in_public_api
+  _WheelComponent createState() => _WheelComponent();
+}
+
+class _WheelComponent extends State<WheelComponent> {
+  StreamController<int> selected = StreamController<int>();
+
+  @override
+  void dispose() {
+    selected.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final items = <String>[
+      'Grogu',
+      'Mace Windu',
+      'Obi-Wan Kenobi',
+      'Han Solo',
+      'Luke Skywalker',
+      'Darth Vader',
+      'Yoda',
+      'Ahsoka Tano',
+    ];
+
+    return Container(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            selected.add(
+              Fortune.randomInt(0, items.length),
+            );
+          });
+        },
+        child: Column(
+          children: [
+            Expanded(
+              child: FortuneWheel(
+                selected: selected.stream,
+                items: [
+                  for (var it in items) FortuneItem(child: Text(it)),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
